6b4eee3ca411068d8aac865258d90c095b3753c6
Ludwing <ludwinggalo6@gmail.com>
2025-11-27 01:53:47 -0600

    Agregue todo los archivos

---
+++ config/credenciales.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "server" => "localhost",
+    "users" => "root",
+    "password" => "Ludwing123",
+    "data" => "bd_mision_verdad"
+];
---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    private $conn;
+    private $server = "localhost";
+    private $user = "root";
+    private $password = "Ludwing123";
+    private $base = "bd_mision_verdad";
+    
+    public function __construct(){
+        $this->conn = new mysqli($this->server, $this->user, $this->password, $this->base);
+
+        if($this->conn->connect_error){
+            echo ("error de conexion");
+        }
+    }
+    
+    public function sendConecction(){
+        return $this->conn;
+    }
+}
